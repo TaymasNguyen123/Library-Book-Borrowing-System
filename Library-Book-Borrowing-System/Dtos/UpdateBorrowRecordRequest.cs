6e44167631ac6e0eabb5a32fe7db855e6ba81fd2
bzee2004 <brianzee2004@gmail.com>
2026-04-17 23:20:50 -0700

    new update borrow record dto and made returnDate optional

--- Library-Book-Borrowing-System/Dtos/UpdateBorrowRecordRequest.cs
+++ Library-Book-Borrowing-System/Dtos/UpdateBorrowRecordRequest.cs
@@ -1,11 +1,9 @@
 namespace Library_Book_Borrowing_System.Dtos;
 
-public class GetBorrowRecordResponse
+public class UpdateBorrowRecordRequest
 {
-    public Guid Id { get; set; }
     public Guid BookId { get; set; }
     public Guid MemberId { get; set; }
-    public DateTime BorrowDate { get; set; }
-    public DateTime? ReturnDate { get; set; }
+    public DateTime ReturnDate { get; set; }
     public required string Status { get; set; }
 }
