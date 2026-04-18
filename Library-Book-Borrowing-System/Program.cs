8b68db664ab552b572db29c2536030addfc21f28
TaymasNguyen123 <tnguyen_csuf@csu.fullerton.edu>
2026-04-18 09:31:47 +0000

    dependancy injection and fix build settings

--- Library-Book-Borrowing-System/Program.cs
+++ Library-Book-Borrowing-System/Program.cs
@@ -16,6 +16,11 @@ builder.Services.AddDbContext<Database>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<Library_Book_Borrowing_System.Services.IBookService, Library_Book_Borrowing_System.Services.BookService>();
+builder.Services.AddScoped<Library_Book_Borrowing_System.Repositories.IBookRepository, Library_Book_Borrowing_System.Repositories.BookRepository>();
+builder.Services.AddScoped<Library_Book_Borrowing_System.Services.IMemberService, Library_Book_Borrowing_System.Services.MemberService>();
+builder.Services.AddScoped<Library_Book_Borrowing_System.Repositories.IMemberRepository, Library_Book_Borrowing_System.Repositories.MemberRepository>();
+builder.Services.AddScoped<Library_Book_Borrowing_System.Services.IBorrowRecordService, Library_Book_Borrowing_System.Services.BorrowRecordService>();
+builder.Services.AddScoped<Library_Book_Borrowing_System.Repositories.IBorrowRecordRepository, Library_Book_Borrowing_System.Repositories.BorrowRecordRepository>();
 
 var app = builder.Build();
 
